012eb0c8bdeba20993e5b3ba81c828a6f675b3af
TalhaIsrar <talhaisrar02@gmail.com>
2025-08-10 22:32:47 +0200

    Added PC logic for instruction fetch in RV32I. TODO: Add branch and jump support

--- src/fetch_stage/pc.v
+++ src/fetch_stage/pc.v
@@ -1,6 +1,16 @@
 module pc(
+    input clk,
+    input rst,
+    input [31:0] next_pc,
+    output reg [31:0] pc
+);
 
-)
-
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            pc <= 32'h00000000; // Reset PC to 0
+        end else begin
+            pc <= next_pc; // Update PC with the next value
+        end
+    end
 
 endmodule
---
+++ src/fetch_stage/pc_update.v
@@ -0,0 +1,11 @@
+module pc_update(
+    input clk,
+    input [31:0] pc,
+
+
+    output [31:0] next_pc
+)
+    assign next_pc = pc + 4; // Increment PC by 4 for next instruction
+    // TODO : Handle branch and jump instructions to update next_pc accordingly
+
+endmodule
