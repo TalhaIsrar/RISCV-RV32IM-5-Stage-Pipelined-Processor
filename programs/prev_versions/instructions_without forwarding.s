1f39e49b8da8548c82072135ae8c8533c90e49f5
TalhaIsrar <talhaisrar02@gmail.com>
2025-08-14 11:36:41 +0200

    Added operand forwarding in register file

--- programs/prev_versions/instructions_without forwarding.s
+++ programs/prev_versions/instructions_without forwarding.s
@@ -11,22 +11,31 @@
     and x7, x2, x4        
 
     # Store/load
-    sw x1, 0(x5)
+    nop
+    sw x1, 0(x5)          
+    nop
+    nop
+    nop
     lw x8, 0(x5)          
 
     addi x9, x6, 10       
 
-    sb x10, 4(x5)
+    sb x10, 4(x5)         
+    nop
+    nop
+    nop
     lb x11, 4(x5)         
 
     # Branch instructions
     beq x1, x2, skip1     
     nop
     nop
+    nop
     addi x12, x0, 1       
 
 skip1:
     bne x1, x2, taken1    
+    nop
     nop
     nop
     addi x13, x0, 2       
@@ -36,6 +45,7 @@ taken1:
 
     # JAL instruction
     jal x15, jump_label   
+    nop
     nop
     nop
     addi x16, x0, 4       
@@ -51,7 +61,6 @@ jump_label:
 
     # AUIPC instruction
     auipc x21, 0x10        # x21 = PC + 0x10000
-    jalr x19, x18, 0
+    nop
 
-    nop
-    nop
+    jalr x19, x18, 0
--- src/decode_stage/register_file.v
+++ src/decode_stage/register_file.v
@@ -13,9 +13,16 @@ module register_file (
 );
     reg [31:0] reg_file [31:0];
 
+    wire [31:0] op1_forwarded;
+    wire [31:0] op2_forwarded;
+
+    // Operand forwarding if read and write addr are same
+    assign op1_forwarded = ((rs1_addr == wr_addr) && wr_en) ? wr_data : reg_file[rs1_addr];
+    assign op2_forwarded = ((rs2_addr == wr_addr) && wr_en) ? wr_data : reg_file[rs2_addr];
+
     // x0 is hardwired to 0
-    assign op1 = (rs1_addr == 0) ? 32'b0 : reg_file[rs1_addr];
-    assign op2 = (rs2_addr == 0) ? 32'b0 : reg_file[rs2_addr];
+    assign op1 = (rs1_addr == 0) ? 32'b0 : op1_forwarded;
+    assign op2 = (rs2_addr == 0) ? 32'b0 : op2_forwarded;
 
     // Write operation
     always @(posedge clk) begin
